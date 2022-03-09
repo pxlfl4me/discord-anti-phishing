e611fb2a6e395161669b899023ff6980e6ed0389
ComicallyBad <50148901+comicallybad@users.noreply.github.com>
2022-03-08 22:35:26 -0500

    Allow

--- function.js
+++ function.js
@@ -1,29 +1,29 @@
-const Discord = require('discord.js')
-const urlJson = require('./json/url.json') 
+const Discord = require('discord.js');
+const urlJson = require('./json/url.json');
 
 exports.antiPhishing = async function(message, embed){
-    if(!message) return console.error('Specify a first parameter (Discord.Message)')
-    if(!embed) return console.error('Specify a second parameter (Discord.MessageEmbed)')
-    if(!message.author && !message.content && !message.member) return console.error('Specify a valid (Discord.Message)')
-    if(embed.length < 2 && !embed.title) return console.error('Specify a valid (Discord.MessageEmbed)')
+    if(!message) return console.error('Specify a first parameter (Discord.Message)');
+    if(!embed) return console.error('Specify a second parameter (Discord.MessageEmbed)');
+    if(!message.author && !message.content && !message.member) return console.error('Specify a valid (Discord.Message)');
+    if(embed.length < 2 && !embed.title) return console.error('Specify a valid (Discord.MessageEmbed)');
 
-    let presente = []
+    let presente = [];
     for(let i = 0; i < urlJson.domains.length; i++){
-        let url = urlJson.domains[i]
+        let url = urlJson.domains[i];
         if(message.content.includes(url)){
-            presente.push(url)
-            break
+            presente.push(url);
+            break;
         }
     }
         
 
     if(presente.length == 1){
-        message.delete()
+        message.delete();
         
-        embed.addField('User:', '`' + message.author.tag + ' | ' + message.author.id + '`')
-        embed.addField('Link:', '`' + presente[0] + '`')
-        embed.setThumbnail(message.author.displayAvatarURL())
+        embed.addField('Link:', '`' + presente[0] + '`');
+        embed.setThumbnail(message.author.displayAvatarURL());
 
-        message.channel.send({embeds: [embed]})
+        message.channel.send({embeds: [embed]});
+        return message;
     }
 }
